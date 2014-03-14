ed0b7639ba4bf146b37eaa586681593ca8977ee1
daniel <xzdasx@gmail.com>
2014-03-14 11:40:32 -0430

    first basic controller shipment_order

--- web/transnode.dart
+++ web/transnode.dart
@@ -8,6 +8,8 @@ import 'package:transnode/routes/transnode_routes.dart' show TransnodeRouter;
 
 import 'package:transnode/controllers/app_controller.dart';
 import 'package:transnode/controllers/login_controller.dart';
+import 'package:transnode/controllers/shipment_order_controller.dart';
+
 
 import 'package:transnode/services/api_service.dart';
 import 'package:transnode/services/user_service.dart';
@@ -21,6 +23,7 @@ class TransnodeModule extends Module {
     type(RouteInitializer, implementedBy: TransnodeRouter);
     type(AppController);
     type(LoginController);
+    type(ShipmentOrderController);
     type(User);
     type(ApiService);
     factory(NgRoutingUsePushState,
