a41cf2a83cdc707978a4738b1a7fad6071e4f5c8
daniel <xzdasx@gmail.com>
2014-03-14 22:28:54 -0430

    basic customer data.

---
+++ lib/controllers/customer_controller.dart
@@ -0,0 +1,9 @@
+library transnode.login_controller;
+import 'package:angular/angular.dart';
+
+@NgController(
+    selector: '[customer-controller]',
+    publishAs: 'customer_controller')
+class CustomerController {
+ 
+}
---
+++ lib/models/customer.dart
@@ -0,0 +1,17 @@
+library transnode.model.customer;
+
+class Customer{
+  String code;
+  String name;
+  String city;
+  String state;
+  String zip;
+  String quote_ref;
+  String bill_estimate;
+  String credit_status;
+  String file_created;
+  String csr_rep;
+  String terms;
+  String customs_broker;
+  String ready_date;
+}
