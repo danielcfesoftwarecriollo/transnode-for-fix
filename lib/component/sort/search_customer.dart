401a6a988a8923a7ff1089620b358ec69b502cd0
Daniel Castillo <danielcfe@softwarecriollo.com>
2014-10-10 19:02:35 -0430

    clear component and fix reference

--- lib/component/sort/search_customer.dart
+++ lib/component/sort/search_customer.dart
@@ -2,13 +2,10 @@ import 'package:polymer/polymer.dart';
 //import '../../showcase/item.dart';
 import 'dart:html';
 import 'package:html_components/html_components.dart' show DatatableComponent, GrowlComponent;
-import 'package:transnode/data/car.dart' as data;
 
 @CustomTag('search-customer')
 class DatatableSearchCustomer extends PolymerElement {
-  
-  @observable List<data.Car> cars = toObservable(data.cars);
-  
+    
   DatatableSearchCustomer.created() : super.created();
   
   void onColumnSorted(CustomEvent event, var detail, DatatableComponent target) {
--- lib/component/sort/sort.dart
+++ lib/component/sort/sort.dart
@@ -2,12 +2,9 @@ import 'package:polymer/polymer.dart';
 //import '../../showcase/item.dart';
 import 'dart:html';
 import 'package:html_components/html_components.dart' show DatatableComponent, GrowlComponent;
-import 'package:transnode/data/car.dart' as data;
 
 @CustomTag('datatable-sort-demo')
 class DatatableSortDemo extends PolymerElement {
-  
-  @observable List<data.Car> cars = toObservable(data.cars);
   
   DatatableSortDemo.created() : super.created();
   
